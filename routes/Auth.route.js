27e26b02e68fcd30f0277de90a31f04fe490c39d
vikas sharma <135947044+vikassharma2004@users.noreply.github.com>
2025-03-04 11:46:38 +0530

    Update Auth.route.js

--- routes/Auth.route.js
+++ routes/Auth.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 const Router = express.Router();
-import { register, login,logout } from "../controllers/Auth.controller.js";
+import { register, login,logout,getprofile } from "../controllers/Auth.controller.js";
 import { authLimiter } from "../middleware/ratelimitter.js";
+import { authMiddleware } from "../middleware/authmiddleware.js";
 
 Router.route("/register").post(authLimiter,register);
 Router.route("/login").post(authLimiter,login);
 Router.route("/logout").post(logout);
+Router.route("/profile").get(authMiddleware,getprofile);
 
 
 
